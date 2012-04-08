cf999bc4c0b1f04b5dfb97b2dc8667dbf12f5726
roy <castasword@gmail.com>
2012-04-08 15:33:01 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Paintingshare::Application.config.secret_token = 'bd5d77270a0c32f4053303818cc7f10c6386079610f3cf9cb0e3c3e1ac6e5edd63c06e842848b11510c468af629f6e96837b8fe6500f6167e74c4d39eeb5a56a'
---
+++ test/unit/user_test.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
